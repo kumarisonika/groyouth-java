6d17a8a2810feed31259a3d0122ee5a2933f4a25
Kumari Sonika <kumarisonika1929@gmail.com>
2026-01-12 18:02:06 +0530

    password encoding done

--- src/main/java/com/groyouth/backend/service/UserService.java
+++ src/main/java/com/groyouth/backend/service/UserService.java
@@ -4,21 +4,27 @@ import com.groyouth.backend.dto.SignupRequest;
 import com.groyouth.backend.model.Role;
 import com.groyouth.backend.model.User;
 import com.groyouth.backend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository= userRepository;
+        this.passwordEncoder=passwordEncoder;
     }
 
     public User signup(SignupRequest request) {
         User user = new User();
         user.setName(request.name);
         user.setEmail(request.email);
-        user.setPassword(request.password);
+
+        user.setPassword(passwordEncoder.encode(request.password));
+
+//        user.setPassword(request.password);
         user.setRole(Role.valueOf(request.role));
         return userRepository.save(user);
     }
